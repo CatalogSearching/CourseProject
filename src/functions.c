d2369ab8a42d392f11fcffcecebbf80b9f43cbdb
Павел Гайдук <gpavel5497@gmail.com>
2017-04-27 21:52:50 +0700

    Ready function getFiles

--- src/functions.c
+++ src/functions.c
@@ -1,11 +1,38 @@
 #include <string.h>
+#include "defines.h"
 #include <dirent.h>
 #include <stdio.h>
-//#define NAMELEN 20
-/*
+int words = 0;
+
+void printToBuf(char files[][NAMELEN], char* way, char* name){
+	char toArr[NAMELEN];
+	strcat(toArr, way);
+	strcat(toArr, "/");
+	strcat(toArr, name);	
+	strcpy(files[words], toArr);
+	words++;
+}
+
 void getFiles(char files[][NAMELEN], char* argv){
-	
-}*/
+	//printf("OKK");
+	int m;
+	DIR* this;
+	char newArg[NAMELEN];
+	this = opendir(argv);
+	struct dirent* h1;
+	while ((h1 = readdir(this)) != NULL){
+		if (h1->d_type == 8){
+			printToBuf(files, argv, h1->d_name);
+		}
+		else if (h1->d_type == 4 && (m = strcmp(h1->d_name, ".")) != 0 && ( m = strcmp(h1->d_name, "..") != 0)){
+			strcpy(newArg, argv);			
+			strcat(newArg, "/");
+			strcat(newArg, h1->d_name);
+			getFiles(files, newArg);
+		}
+		
+	}
+}
 /*
 void searchStr(char* file, char* argv){
 	
--- src/main.c
+++ src/main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
-
-int main(){
+#include <string.h>
+#include "defines.h"
+#include "functions.h"
+int main(int argc, char * argv[]){
+	extern int words;
+	char files[FILESC][NAMELEN];
+	for (int i = 0; i < FILESC; i++){
+		for (int j = 0; j < FILESC; j++){
+			files[i][j] = '\0';
+		}
+	}	
+	printf("Search in : %s\n", argv[1]);
+	printf("%c\n", files[2][2]);
+	getFiles(files, argv[1]);
+	for (int y = 0; y < words; y++){
+		printf("%s\n", files[y]);
+	}
 	return 0;
 }
