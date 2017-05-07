238ad489e6d33d3005042d64eef4a9ead97db174
Petruk Egor <Petruk.egor@hotmail.com>
2017-05-07 21:16:09 +0700

    First commit, edit main.c

--- src/main.c
+++ src/main.c
@@ -2,21 +2,14 @@
 #include <string.h>
 #include "defines.h"
 #include "functions.h"
-
-int main(const int argc, const char *argv[]){
-
+int main(int argc, char * argv[]){
 	extern int words;
-
-	check_arg(argc, argv[]); //Проверка аргументов командной строки
-
 	char files[FILESC][NAMELEN];
-
 	for (int i = 0; i < FILESC; i++){
 		for (int j = 0; j < FILESC; j++){
 			files[i][j] = '\0';
 		}
 	}
-
 	printf("Search in : %s\n", argv[1]);
 	printf("%c\n", files[2][2]);
 	getFiles(files, argv[1]);
