e896b5e275753cf1938604ddf0f331f77476f1a3
Petruk Egor <Petruk.egor@hotmail.com>
2017-05-12 22:50:33 +0700

    Add color printing and fix some bugs

--- src/functions.c
+++ src/functions.c
@@ -3,7 +3,9 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 int words = 0;
+char colors[][5] = {"0;31","0;32"};
 
 void printToBuf(char files[][NAMELEN], char* way, char* name){
 	char toArr[NAMELEN];
@@ -11,7 +13,7 @@ void printToBuf(char files[][NAMELEN], char* way, char* name){
 		toArr[i] = '\0';
 	strcat(toArr, way);
 	strcat(toArr, "/");
-	strcat(toArr, name);	
+	strcat(toArr, name);
 	strcpy(files[words], toArr);
 	words++;
 }
@@ -20,41 +22,61 @@ void getFiles(char files[][NAMELEN], char* argv){
 	int m;
 	char newArg[NAMELEN];
 	this = opendir(argv);
+	if (this == NULL){
+		printf("%s Нет такого файла или директории\n", argv);
+		return;
+	}
 	struct dirent* h1;
 	while ((h1 = readdir(this)) != NULL){
 		if (h1->d_type == 8){
 			printToBuf(files, argv, h1->d_name);
 		}
 		else if (h1->d_type == 4 && (m = strcmp(h1->d_name, ".")) != 0 && ( m = strcmp(h1->d_name, "..") != 0)){
-			strcpy(newArg, argv);			
+			strcpy(newArg, argv);
 			strcat(newArg, "/");
 			strcat(newArg, h1->d_name);
 			getFiles(files, newArg);
 		}
-		
+
 	}
 }
-
+/*
+finder() - функция поиска всех вхождений подстроки find в строке string
+В случае успеха возвращает количество вхождений, иначе 0
+*/
 int finder(char *string, char* find){
+
     int flag = 0;
-    char *p;
-    p = strstr(string, find);
+    char *p = strstr(string, find);
+
     if (p == NULL){
         return flag;
-    }else{
+    } else {
         flag ++;
         string = p + strlen(find);
         finder(string, find);
     }
     return 1;
 }
+/*
+searchStr - функция поиска и вывода строк, из файла, в которых найдены подстроки strFnd
+file - путь к файлу
+*/
 void searchStr(char* file, char* strFnd){
+
     char strSrc[1000];
     char *estr;
     int i = 0, j = 0, counter = 0;
-    printf("____\nПоиск в файле: %s\n", file);
+
+    printf("Поиск в файле: %s\n", file);
 
     FILE *infile = fopen(file, "r");
+
+	if (infile == NULL){
+		printf("Файл не найден\n");
+		return;
+	}
+
     while(!feof(infile)){
         counter++;
         estr = fgets(strSrc, 999, infile);
@@ -65,15 +87,27 @@ void searchStr(char* file, char* strFnd){
         i = finder(strSrc, strFnd);
         if(i != 0){
             j++;
-            printf("Найдены соответствия в %d строке\n", counter);
+            printf("Найдены соответствия в %d-й строке: ", counter);
+			strSrc[strlen(strSrc) - 1] = '\0';
             printf("%s\n", strSrc);
         }
 
     }
+
     if(j != 0){
-        printf("[OK]\n");
+		printf("[");
+		printf("%s%sm",CSI,colors[GREEN]);
+	    printf("OK");
+	    printf("%s0m",CSI);
+		printf("]\n\n");
     } else {
-        printf("Соответствий не найдено\n[FAIL]\n");
+        printf("Соответствий не найдено\n");
+		printf("[");
+		printf("%s%sm",CSI,colors[RED]);
+	    printf("FALSE");
+	    printf("%s0m",CSI);
+		printf("]\n\n");
     }
+
     fclose(infile);
 }
