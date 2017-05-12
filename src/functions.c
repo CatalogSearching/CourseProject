e9e181fc37b2677c903d40e6ff46120108cfedb1
Гайдук Павел Александрович <iv622s02@lwp09.csc.local>
2017-05-12 15:13:23 +0700

    Preparing

--- src/functions.c
+++ src/functions.c
@@ -1,12 +1,4 @@
 #include <string.h>
 #include <dirent.h>
 #include <stdio.h>
-//#define NAMELEN 20
-/*
-void getFiles(char files[][NAMELEN], char* argv){
-	
-}*/
-/*
-void searchStr(char* file, char* argv){
-	
-}*/
+
