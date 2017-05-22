ec186af3741423dad999d8d6a03087a7bdba916a
Павел Гайдук <gpavel5497@gmail.com>
2017-05-22 13:46:33 +0700

    Tests fixed

--- test/functions_test.c
+++ test/functions_test.c
@@ -3,30 +3,32 @@
 #include "../src/functions.h"
 #include "../src/defines.h"
 
-
+char vals[5][NAMELEN];
+CTEST(GetFiles, GetValue){
+	int v1 = getFiles(vals, "Direct");
+	const int exp = 5;
+	ASSERT_EQUAL(exp, v1);
+}
 CTEST(GetFiles, File_1){
-	const char exp[] = "Direct/AgainText.txt";
-	extern char vals[5][NAMELEN];
+	for (int i = 0; i < 5; i++)
+		printf("%s\n", vals[i]);
+	const char exp[] = "Direct/text.txt";
 	ASSERT_STR(exp, vals[0]);
 }
 CTEST(GetFiles, File_2){
-	const char exp[] = "Direct/text.txt";
-	extern char vals[5][NAMELEN];
+	const char exp[] = "Direct/anotherDir/MoreTexts.txt";
 	ASSERT_STR(exp, vals[1]);
 }
 CTEST(GetFiles, File_3){
-	const char exp[] = "Direct/OneMoreDir/text.txt";
-	extern char vals[5][NAMELEN];
+	const char exp[] = "Direct/anotherDir/textFile.txt";
 	ASSERT_STR(exp, vals[2]);
 }
 CTEST(GetFiles, File_4){
-	const char exp[] = "Direct/anotherDir/MoreTexts.txt";
-	extern char vals[5][NAMELEN];
+	const char exp[] = "Direct/AgainText.txt";
 	ASSERT_STR(exp, vals[3]);
 }
 CTEST(GetFiles, File_5){
-	const char exp[] = "Direct/anotherDir/textFile.txt";
-	extern char vals[5][NAMELEN];
+	const char exp[] = "Direct/OneMoreDir/text.txt";
 	ASSERT_STR(exp, vals[4]);
 }
 CTEST(GetFiles, Filedoesntexist){
@@ -35,13 +37,6 @@ CTEST(GetFiles, Filedoesntexist){
 	int val = getFiles(vl, "ifExist");
 	ASSERT_EQUAL(val, exp);
 }
-/*CTEST(GetFiles, GetValue){
-	const int exp = 5;
-	char arr[6][NAMELEN];
-	int v1;
-	v1 = getFiles(arr, "Direct");
-	ASSERT_EQUAL(exp, v1);
-}*/
 char src1[] = "This is testsource string 1";
 char src2[] = "This is test source str1ng 2";
 char src3[] = "This is test source string string string 3";
--- test/main.c
+++ test/main.c
@@ -2,8 +2,7 @@
 #define CTEST_SEGFAULT
 #include "../thirdparty/ctest.h"
 #include "../src/functions.h"
-char vals[5][NAMELEN];
+
 int main(int argc, const char** argv){
-	getFiles(vals, "Direct");
 	return ctest_main(argc, argv);
 }
